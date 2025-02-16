de7c31411405a7e394027e3a68325bcd3441986d
Amaan Marfatia <amaanmarfatia@gmail.com>
2025-02-15 23:54:06 -0500

    Don't have rated puzzle when hint is used

--- lib/src/model/puzzle/puzzle_controller.dart
+++ lib/src/model/puzzle/puzzle_controller.dart
@@ -107,6 +107,7 @@ class PuzzleController extends _$PuzzleController {
       node: _gameTree.view,
       pov: _gameTree.nodeAt(initialPath).position.ply.isEven ? Side.white : Side.black,
       canViewSolution: false,
+      hintShown: false,
       showHint: false,
       resultSent: false,
       isChangingDifficulty: false,
@@ -221,7 +222,7 @@ class PuzzleController extends _$PuzzleController {
     if (state.showHint) {
       final NormalMove move = solutionMove();
       final ISet<Square>? possibleMoves = state.validMoves.get(move.from);
-      state = state.copyWith(hintMove: move, hintPossibleMoves: possibleMoves);
+      state = state.copyWith(hintShown: true, hintMove: move, hintPossibleMoves: possibleMoves);
     }
   }
 
@@ -339,7 +340,10 @@ class PuzzleController extends _$PuzzleController {
         solution: PuzzleSolution(
           id: state.puzzle.puzzle.id,
           win: state.result == PuzzleResult.win,
-          rated: initialContext.userId != null && ref.read(puzzlePreferencesProvider).rated,
+          rated:
+              initialContext.userId != null &&
+              !state.hintShown &&
+              ref.read(puzzlePreferencesProvider).rated,
         ),
       );
 
@@ -540,6 +544,7 @@ class PuzzleState with _$PuzzleState {
     PuzzleFeedback? feedback,
     required bool canViewSolution,
     required bool showHint,
+    required bool hintShown,
     NormalMove? hintMove,
     ISet<Square>? hintPossibleMoves,
     required bool isLocalEvalEnabled,
